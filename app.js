6aa2b062ca836fe943827fb61a7528a4e9291f7c
Nguyen Huyen <nguyenhuyen@MacBook-Pro-cua-Nguyen.local>
2024-05-31 21:21:19 -0700

    add

--- app.js
+++ app.js
@@ -46,6 +46,9 @@ app.post('/register', async(req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+app.use((req,res,next) => {
+    res.status(404).sendFile(__dirname + '/views/error.html');
+});
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
