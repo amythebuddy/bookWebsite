d1928ab1764088ba15d18be619d90d0bb2ea21b4
hacnguyen <vanhacnguyen1708@gmail.com>
2024-09-09 19:42:53 -0700

    add

--- public/js/bookshelf.js
+++ public/js/bookshelf.js
@@ -5,15 +5,16 @@ async function favoriteBook(){
     try{
         const response = await fetch('http://localhost:3000/api/books');
         const data = await response.json();
-
+        console.log(data);
         const tableRow = table.insertRow(); //adding new row for new favorite book
         for(const key in data){
-            const bookCover = tableRow.insertCell(0);
+            const bookCover = tableRow.insertCell(0); 
             const title = tableRow.insertCell(1);
             bookCover.innerText = key.cover;
             title.innerText = key.bookName;
+            
         }
     } catch {
-
+        console.error("Error displaying: ", error);
     }
 }
